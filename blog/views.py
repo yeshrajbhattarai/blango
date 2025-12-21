585f345b6e4aa85485dbfd6ed2e703137dbcdedc
Yesh Bhattarai <yeshraj_202422019@smit.smu.edu.in>
2025-12-21 10:14:42 +0000

    Finished custom Filters

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
+from django.utils import timezone
+from blog.models import Post
+
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    posts = Post.objects.filter(published_at__lte=timezone.now())
+    return render(request, "blog/index.html", {"posts": posts})
